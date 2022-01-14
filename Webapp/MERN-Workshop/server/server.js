9487801878bde9ab679c4ef06c199bb4b166340e
pongsakorn <best.mbed@gmail.com>
2022-01-14 17:39:35 +0700

    disable port

--- Webapp/MERN-Workshop/server/server.js
+++ Webapp/MERN-Workshop/server/server.js
@@ -19,6 +19,6 @@ app.get("*",(req,res)=>{
     })
 })
 
-const port = process.env.PORT || 8080
+//const port = process.env.PORT || 8080
 
 app.listen(port,()=>console.log(`start server by port ${port}`))
